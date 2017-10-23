18714d48946754172a456d1a2f25d6300663dd56
Brendan Cronan <32400374+brendan-cronan@users.noreply.github.com>
2017-10-23 15:30:01 -0400

    Revert "Revert "Basic framework""
    This reverts commit 12d7a6777f99daba1b6e3a06055499038410cae3.

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -2,26 +2,58 @@ package game;
 
 public class Cell {
 
-	private Vector pos=Vector.DEFAULT;
+	private Vector pos = Vector.DEFAULT;
 	private Troop troop;
-	
-	
-	public Cell(int x,int y) {
-		pos=new Vector(x,y);
+
+	public Cell(int x, int y) {
+		pos = new Vector(x, y);
 	}
-	public Cell(int x,int y,Troop t) {
-		troop=t;
-		pos=new Vector(x,y);
+
+	public Cell(int x, int y, Troop t) {
+		troop = t;
+		pos = new Vector(x, y);
 	}
-	
-	
-	
-	
+
 	public Cell(Vector position) {
-		pos=new Vector(position);
+		pos = new Vector(position);
 	}
-	public Cell(Vector position,Troop t) {
-		troop=t;
-		pos=new Vector(position);
+
+	public Cell(Vector position, Troop t) {
+		troop = t;
+		pos = new Vector(position);
 	}
+
+	public Vector getPos() {
+		return pos;
+	}
+	
+	public Troop getTroop() {
+		return troop;
+	}
+	public Troop removeTroop() {
+		Troop temp=troop;
+		troop=null;
+		return temp;
+	}
+	/*
+	 * @return the Troop that was in the Cell previously.Null if unOccupied.
+	 * 
+	 */
+	public Troop addTroop(Troop newTroop) {
+		if(troop==null) {
+			troop=newTroop;
+			return null;
+		}
+		else {
+			Troop temp=troop;
+			troop=newTroop;
+			return temp;
+		}
+		
+	}
+	
+	public boolean isEmpty() {
+		return (troop == null) ? true : false;
+	}
+
 }
--- src/game/Troop.java
+++ src/game/Troop.java
@@ -9,5 +9,9 @@ public abstract class Troop {
 		
 	
 	}
+	
+	public Vector getPos() {
+		return pos;
+	}
 
 }

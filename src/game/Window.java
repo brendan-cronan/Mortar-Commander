cd66eae7b099c04b31bb3bf4bd220d67cf44f1ef
Jordan Ward <wardjor@mail.gvsu.edu>
2017-11-29 15:44:23 -0500

    More GUI

--- src/game/Window.java
+++ src/game/Window.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,16 +10,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
+@SuppressWarnings("serial")
 public class Window extends JPanel{
 	
 	private JButton[][] board;
-	private static JFrame frame;
+	private static JFrame boardFrame;
+	private static JPanel commandFrame;
 	private static int sizeOfBoard;
-	
+	private static ButtonListener buttonListener;
 	
 	public Window(GameState state) {
 		board = new JButton[state.game.length][state.game[0].length];
-		ButtonListener buttonListener = new ButtonListener();
+		buttonListener = new ButtonListener();
 		setLayout(new GridLayout(state.game.length, state.game[0].length));
 		for (int i = 0; i < board.length; i++){
 			for (int j = 0; j < board[i].length; j++){
@@ -31,13 +35,38 @@ public class Window extends JPanel{
 	}
 	
 	public static void main(String[] args){
-		 frame = new JFrame("Mortar Commander");
-		 sizeOfBoard = 10;
-		 GameState state = new GameState(sizeOfBoard);
-		 Window window = new Window(state);
-		 frame.add(window);
-		 frame.pack();
-		 frame.setVisible(true);
+		
+		// create the window
+		boardFrame = new JFrame("Mortar Commander");
+		boardFrame.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		boardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// DO_NOTHING_ON_CLOSE
+		
+		// initialize and add game board
+		sizeOfBoard = 10;
+		GameState state = new GameState(sizeOfBoard);
+		Window window = new Window(state);
+		boardFrame.add(window, constraints);
+		
+		// add some command buttons
+		commandFrame = new JPanel();
+		JButton endTurn = new JButton("End Turn");
+		endTurn.addActionListener(buttonListener);
+		endTurn.setActionCommand("endTurn");
+		commandFrame.add(endTurn);
+		JButton attack = new JButton("Attack");
+		attack.addActionListener(buttonListener);
+		attack.setActionCommand("attack");
+		commandFrame.add(attack);
+		JButton move = new JButton("Move");
+		move.addActionListener(buttonListener);
+		move.setActionCommand("move");
+		commandFrame.add(move);
+		constraints.gridy = 1;
+		boardFrame.add(commandFrame, constraints);
+		
+		boardFrame.pack();
+		boardFrame.setVisible(true);
 	}
 	
 	private class ButtonListener implements ActionListener {

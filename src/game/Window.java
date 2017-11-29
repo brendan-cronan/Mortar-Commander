9309da4131b83ff8db06bcf340ad8de8e95794c6
SchindlerDan <schindld@mail.gvsu.edu>
2017-11-29 16:02:46 -0500

    copy/pasted window from danGameLogic branch -Dan

---
+++ src/game/Window.java
@@ -0,0 +1,79 @@
+package game;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class Window extends JPanel{
+	
+	private JButton[][] board;
+	private static JFrame boardFrame;
+	private static JPanel commandFrame;
+	private static int sizeOfBoard;
+	private static ButtonListener buttonListener;
+	
+	public Window(int size) {
+		board = new JButton[sizeOfBoard][sizeOfBoard];
+		buttonListener = new ButtonListener();
+		setLayout(new GridLayout(sizeOfBoard, sizeOfBoard));
+		for (int i = 0; i < board.length; i++){
+			for (int j = 0; j < board[i].length; j++){
+				board[i][j] = new JButton();
+				board[i][j].addActionListener(buttonListener);
+				board[i][j].setActionCommand(i + " " + j);
+				add(board[i][j]);
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		
+		// create the window
+		boardFrame = new JFrame("Mortar Commander");
+		boardFrame.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		boardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// DO_NOTHING_ON_CLOSE
+		
+		// initialize and add game board
+		sizeOfBoard = 10;	// FIXME change to get user input
+		Window window = new Window(sizeOfBoard);
+		boardFrame.add(window, constraints);
+		
+		// add some command buttons
+		commandFrame = new JPanel();
+		JButton endTurn = new JButton("End Turn");
+		endTurn.addActionListener(buttonListener);
+		endTurn.setActionCommand("endTurn");
+		commandFrame.add(endTurn);
+		JButton attack = new JButton("Attack");
+		attack.addActionListener(buttonListener);
+		attack.setActionCommand("attack");
+		commandFrame.add(attack);
+		JButton move = new JButton("Move");
+		move.addActionListener(buttonListener);
+		move.setActionCommand("move");
+		commandFrame.add(move);
+		constraints.gridy = 1;
+		boardFrame.add(commandFrame, constraints);
+		
+		boardFrame.pack();
+		boardFrame.setVisible(true);
+	}
+	
+	private class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			System.out.println(arg0.getActionCommand());
+		}
+	}
+
+}

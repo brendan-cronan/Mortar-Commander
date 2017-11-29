45e8310c292f8d5dd28c66ad8df83139a74a82d2
Brendan <Brendan@35.40.126.6>
2017-11-29 16:56:50 -0500

    maybe?

--- src/game/server.java
+++ src/game/server.java
@@ -4,100 +4,75 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class server {
-	private final static int  PORT = 6603;
+	private final static int PORT = 6603;
 	private static ServerSocket welcome;
 	private static Socket player;
 	public static int turn = 0, numPlayers = 0;
 	public static game game = new game(false);
 
 	public static boolean gameOn = true;
-	
-	public static void main(String args[]){
-		try{
+
+	public static void main(String args[]) {
+		try {
 			welcome = new ServerSocket(PORT);
-			while(true){
+			while (true) {
 				player = welcome.accept();
 				System.out.println("connected successfully");
 				numPlayers++;
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+
 			}
-		
-		
-		
-		
-	}catch(Exception e){
-		
+
+		} catch (Exception e) {
+
+		}
+
 	}
-	
 
 }
-	
-	
-	
-}
 
+class clientHandler implements Runnable {
 
-class clientHandler implements Runnable{
-	
 	Socket player;
 	String playerName;
 	int port;
 	int playerNum;
 	ArrayList<soldier> playerSoldiers;
-	
-	
-	public clientHandler(Socket socket, int playerNum){
+
+	public clientHandler(Socket socket, int playerNum) {
 		this.playerNum = playerNum;
 		player = socket;
 	}
-	
-	public void run(){
+
+	public void run() {
 		int[] move, livingTroops;
 		soldier current;
-		try{
-			while(server.gameOn) {
-				
-				
-				while(server.turn%server.numPlayers != playerNum);
+		try {
+			while (server.gameOn) {
+
+				while (server.turn % server.numPlayers != playerNum)
+					;
 				Net_Util.send(player, server.gameOn);
 				livingTroops = server.game.getLivingTroops(playerNum);
 				Net_Util.send(player, livingTroops);
 
 				playerSoldiers = server.game.getPlayerTroops().get(playerNum);
-				for(int i = 0; i < playerSoldiers.size(); i++) {
-					
-				move = Net_Util.recIntArr(player);
-				for(soldier s:playerSoldiers) {
-					if(s.getX() == move[0] && s.getY() == move[1]) {
-						current = s;
+				for (int i = 0; i < playerSoldiers.size(); i++) {
+
+					move = Net_Util.recIntArr(player);
+					for (soldier s : playerSoldiers) {
+						if (s.getX() == move[0] && s.getY() == move[1]) {
+							current = s;
+						}
 					}
+
 				}
-					
 				
-			
-			
-			
-			
-			
-			
+				
 			}
 			Net_Util.send(player, server.gameOn);
 			player.close();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
-		
-		

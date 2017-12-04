ab33fcf6c0de4fb56592dff215d02f1929bae0ae
SchindlerDan <schindld@mail.gvsu.edu>
2017-12-04 18:49:26 -0500

    fixed up the server class -Dan Schindler

--- src/game/server.java
+++ src/game/server.java
@@ -13,10 +13,10 @@ public class server {
 	private static int players = 0;
 	public static boolean gameOn = false;
 	public static game board;
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		ArrayList<ClientHandler> playerList = new ArrayList<ClientHandler>();
-		try{
+		try {
 
 			welcome = new ServerSocket(PORT);
 			while (true) {
@@ -28,44 +28,38 @@ public class server {
 
 				new Thread(handler).start();
 				players++;
-					
-				//this should allow us to start a new game once everyone has selected that they are ready
-				if(playerReady > 0 && playerReady == players){
+
+				// this should allow us to start a new game once everyone has
+				// selected that they are ready
+				if (playerReady > 0 && playerReady == players) {
 					board = new game();
 					gameOn = true;
 					break;
 				}
-				
-				
 
 			}
-		board.NumPlayers(players);
-		board.placeTroopsBoring();
-		int winner = board.checkWinner();
-		while(winner < 0){
-			for(int i = 0; i < players; i++){
-				playerTurn = i;
-				while(playerList.get(i).getPlaying()){
-					
-					
+			board.NumPlayers(players);
+			board.placeTroopsBoring();
+			int winner = board.checkWinner();
+			while (winner < 0) {
+				for (int i = 0; i < players; i++) {
+					playerTurn = i;
+					while (playerList.get(i).getPlaying()) {
+
+					}
+					winner = board.checkWinner();
 				}
-				winner = board.checkWinner();	
+
 			}
-			
+
+		} catch (Exception e) {
+
 		}
-		
-		
-	}catch(Exception e){
-		
 
 	}
-
-}
 }
 
-
-
-class ClientHandler implements Runnable{
+class ClientHandler implements Runnable {
 	boolean playing = false;
 
 	Socket player;
@@ -73,92 +67,92 @@ class ClientHandler implements Runnable{
 	int port;
 
 	int playerNumber;
-	public ClientHandler(Socket socket, int playerNum){
+
+	public ClientHandler(Socket socket, int playerNum) {
 		playerNumber = playerNum;
 
 		player = socket;
 
 	}
 
-	
-	public boolean getPlaying(){
+	public boolean getPlaying() {
 		return playing;
 	}
 
 	@Override
 	public void run() {
 		boolean running = true;
-	
+
 		String[] command;
-		
-		
-		while(running){
-			try{
-				if(playerNumber == server.playerTurn && server.board.getLivingTroops(playerNumber).length > 0){
+
+		while (running) {
+			try {
+				if (playerNumber == server.playerTurn && server.board.getLivingTroops(playerNumber).length > 0) {
 					playing = true;
 					startPlayerTurn();
-					
+
 				}
-				
+
 				command = Net_Util.recString(player).split(" ");
-				//has the game started yet? if not, let's check if this player
-				//is ready to begin. If so, increment the number of ready players.
-				if(!server.gameOn && command[0].equalsIgnoreCase("ready")){
+				// has the game started yet? if not, let's check if this player
+				// is ready to begin. If so, increment the number of ready
+				// players.
+				if (!server.gameOn && command[0].equalsIgnoreCase("ready")) {
 					server.playerReady++;
 				}
-				if(playerNumber == server.playerTurn && server.board.getLivingTroops(playerNumber).length > 0){
-					//playing = true;
-					//startPlayerTurn();
-					
-					
-				//each player will get three actions (move or attack) regardless 
-				//of the number of troops
-					for(int x = 0; x < 3; x++){
-					if(command[0].startsWith("move")){
-						int[] coordinates = new int[command.length - 2];
-						for(int i = 0; i < command.length; i++ ){
-							if(i == 0){
-							
-							}else{
-								coordinates[i-1] = Integer.parseInt(command[i]);
-							}
-						//once we create the board, here's where we'll first convert
-						//the coordinates into a soldier and then move that soldier
-							soldier mover = server.board.getTroop(coordinates[0], coordinates[1]);
-							server.board.move(mover, coordinates[2], coordinates[3]);
-							Net_Util.send(player, server.board.lineOfSight(mover));
-						}
-					}else if(command[0].startsWith("attack")){
-					
-						int[] coordinates = new int[command.length - 2];
-						for(int i = 0; i < command.length; i++ ){
-							if(i == 0){
-							
-							}else{
-								coordinates[i-1] = Integer.parseInt(command[i]);
-							}
-						}
-						server.board.attack(coordinates[1], coordinates[2]);
+				if (playerNumber == server.playerTurn && server.board.getLivingTroops(playerNumber).length > 0) {
+					// playing = true;
+					// startPlayerTurn();
 
-					}		
-					command = Net_Util.recString(player).split(" ");
+					// each player will get three actions (move or attack)
+					// regardless
+					// of the number of troops
+					for (int x = 0; x < 3; x++) {
+						if (command[0].startsWith("move")) {
+							int[] coordinates = new int[command.length - 2];
+							for (int i = 0; i < command.length; i++) {
+								if (i == 0) {
+
+								} else {
+									coordinates[i - 1] = Integer.parseInt(command[i]);
+								}
+								// once we create the board, here's where we'll
+								// first convert
+								// the coordinates into a soldier and then move
+								// that soldier
+								soldier mover = server.board.getTroop(coordinates[0], coordinates[1]);
+								server.board.move(mover, coordinates[2], coordinates[3]);
+								Net_Util.send(player, server.board.lineOfSight(mover));
+							}
+						} else if (command[0].startsWith("attack")) {
+
+							int[] coordinates = new int[command.length - 2];
+							for (int i = 0; i < command.length; i++) {
+								if (i == 0) {
+
+								} else {
+									coordinates[i - 1] = Integer.parseInt(command[i]);
+								}
+							}
+							server.board.attack(coordinates[1], coordinates[2]);
+
+						}
+						command = Net_Util.recString(player).split(" ");
 					}
 
-				}else if(server.board.getLivingTroops(playerNumber).length == 0){
+				} else if (server.board.getLivingTroops(playerNumber).length == 0) {
 					playing = false;
 				}
-	
-			}catch(Exception e){
-				
+
+			} catch (Exception e) {
+
 			}
 		}
-		
+
 	}
-	
-	public void startPlayerTurn(){
+
+	public void startPlayerTurn() {
 		Net_Util.send(player, server.board.checkWinner() < 0);
-		Net_Util.send(player, server.board.getLivingTroops(playerNumber));	
+		Net_Util.send(player, server.board.getLivingTroops(playerNumber));
 	}
 }
-
-
